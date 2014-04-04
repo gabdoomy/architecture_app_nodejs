e141f3a267270e455f08057403e46b7ef95b430e
Alex <ad1444@my.bristol.ac.uk>
2014-04-04 03:29:30 +0100

    show projects integrated

--- arch_database/public/js/blueprints.js
+++ arch_database/public/js/blueprints.js
@@ -1,70 +1,71 @@
  function showImg(){
  	var pagename =document.location.href.match(/[^\/]+$/)[0].split(".")[0];
-	var images =new Array();
 
 	var category = 4;
 	if(pagename.localeCompare("commercial")==0) category=1;
 	else if(pagename.localeCompare("residential")==0) category=2;
 	else if(pagename.localeCompare("educational")==0) category=3;
-	$.getJSON("/query?category="+category, function( data ) {  	console.log("h-"+data);
-  	$.each(data, function(name, d_name){
-  		console.log(d_name);
-  		//console.log(d_name.Name);
-  	});
-	});
-	console.log("here");
-	images[0]=0;
-	images[1]=1;
 	
 	var im;
-	for(var i=0;i<images.length;i++){
-		im = new String("./images/blueprints/blueprint_"+pagename+"_");
-		var $proj=$( "<div class=\"panel panel-default\"></div>" );
-		
-		$proj.append(
-			"<div class=\"panel-heading\"> Project "+(images[i]+1)+"</div>"
-			+"<div class=\"panel-body\">"
-				+"<div>"
-					+"<div id =\"imageinsert"+images[i]+"\">"
-						+"<img id=\"zoom_"+images[i]+"\" class= \"zoom project_info\" src=\""+im+"small_"+images[i]+".png\" data-zoom-image=\""+im+images[i]+".png\"></img>"
+	$.getJSON("/query?category="+category, function( data ) {
+		var i=0;
+  	$.each(data, function(name, d_name){
+			im = new String("./images/blueprints/blueprint_"+pagename+"_");
+			var $proj=$( "<div class=\"panel panel-default\"></div>" );
+			
+			$proj.append(
+				"<div class=\"panel-heading\"> Project "+d_name.Name+"</div>"
+				+"<div class=\"panel-body\">"
+					+"<div>"
+						+"<div id =\"imageinsert"+i+"\">"
+							+"<img id=\"zoom_"+i+"\" class= \"zoom project_info\" src=\""+im+i+".png\" data-zoom-image=\""+im+i+".png\"></img>"
+						+"</div>"
+						+"<div id=\"info_"+i+"\" class = \"project_info\"></div>"
 					+"</div>"
-					+"<div id=\"info_"+images[i]+"\" class = \"project_info\"></div>"
+					+"<hr/>"
+					+"<div class =\"project_date\">Posted : "+d_name.Date+"</div>"
 				+"</div>"
-				+"<hr/>"
-				+"<div class =\"project_date\">Posted : 17 July 2014 13:04</div>"
-			+"</div>"
-			);
-	
-		$proj.appendTo( "#projects" );
-	}
-	for(var i=0;i<images.length;i++){
-		var $price=$("<div id=\"price\">Price: &#163;"+(images[i]+100)+"</div><br/>");
-		$price.appendTo( "#imageinsert"+images[i] );
+				);
 		
-		var $image=$("<a href=\"model.html?name="+pagename+"_model"+images[i]+"\"><img id=\"button_3d\" src=\"./images/button_3d.png\" alt=\"\" name=\"name\" value=\"treehouse_logo\"/></a>");
-		$image.appendTo( "#imageinsert"+images[i] );
-		
-		var $text=$("<div>Project: Info project "+images[i]+"</div>"+
-					"<div>Uploaded by: Info uploader "+images[i]+"</div>"+
-					"<div>Levels: Levels info project "+images[i]+"</div>"+
-					"<div>Contact info: To be completed - info "+images[i]+"</div>"+
-					"<div>Info project: project "+images[i]+"</div>"
-					);
-		$text.appendTo( "#info_"+images[i] );
-	}
+			$proj.appendTo( "#projects" );
+
+			var $price=$("<div id=\"price\">Price: &#163;"+d_name.Price+"</div><br/>");
+			$price.appendTo( "#imageinsert"+i );
+			
+			var $image=$("<a href=\"model.html?name="+pagename+"_model"+i+"\"><img id=\"button_3d\" src=\"./images/button_3d.png\" alt=\"\" name=\"name\" value=\"treehouse_logo\"/></a>");
+			$image.appendTo( "#imageinsert"+i );
+			
+			var $text=$("<div>Project Name: "+d_name.Name+"</div>"+
+						"<div>Uploaded by: "+d_name.User+"</div>"+
+						"<div>Levels: "+d_name.Levels+"</div>"+
+						"<div>Contact info: "+d_name.Contact+"</div>"+
+						"<div>Info project: "+d_name.Info+"</div>"
+						);
+			$text.appendTo( "#info_"+i );
+
+			i=i+1;
+			//console.log(i);
+		});
+	});
 }
 
 $( document ).ready(function() {
 	showImg();
+	var pagename =document.location.href.match(/[^\/]+$/)[0].split(".")[0];
+	var category = 4;
+	if(pagename.localeCompare("commercial")==0) category=1;
+	else if(pagename.localeCompare("residential")==0) category=2;
+	else if(pagename.localeCompare("educational")==0) category=3;
 
-	$("#zoom_0").elevateZoom({
-	  zoomType				: "inner",
-	  cursor: "crosshair"
-	});
-
-	$("#zoom_1").elevateZoom({
-	  zoomType				: "inner",
-	  cursor: "crosshair"
-	});
+	$.getJSON("/query?category="+category, function( data ) {
+		var i=0;
+  	$.each(data, function(name, d_name){
+			$("#zoom_"+i).elevateZoom({
+			  zoomType				: "inner",
+			  cursor: "crosshair"
+			});
+			i=i+1;
+		});
+  });
 
 });
